d3ad94c889decbb5042f43b9da2d87dc6da81f19
Hasanat Jahan <jahanhasanat@gmail.com>
2020-12-05 22:12:13 -0500

    getting 400 error with this shibe image sample

--- shibe_img_get_sample.js
+++ shibe_img_get_sample.js
@@ -1,53 +1,71 @@
 var https = require('https');
 var fs = require('fs');
+const qs = require("querystring");
 
 let download_images = 0;
 let album_art_paths = [];
 let image_url = "https://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true";
-	// create a request for the images 
-	// let image_req = https.get(image_url, function(image_res){
-	// 	// console.log(image_url.slice(24));
-	// 	// let slice_url = image_url.slice(24);
-	// 	console.log(image_res);
-	// 	// let new_img= fs.createWriteStream(`./shibe_img/${slice_url}.png`, {'encoding':null});
-	// 	// image_res.pipe(new_img);
-	// 	// album_art_paths.push(`./album-art/${slice_url}.png`);
 
-	// 	// new_img.on("finish", function(){
-	// 	// 	download_images++;
-	// 	// 	if(download_images === album_items_length){
-	// 	// 		console.log(album_art_paths);
-	// 	// 		// generate_webpage(res, album_art_paths);
-	// 	// 		// reset download images for each request 
-	// 	// 		download_images = 0;
-	// 	// 		// album_art_paths = [];
-	// 	// 	}
-	// 	// });
-	// });
 
-	// image_req.on("error", function(err){
-	// 	console.log(err);
-	// });
+const shibe_img_request = https.get(image_url, (incoming_message) =>{
+    const chunks = [];
+    incoming_message.on("data", (data) =>{
+        chunks.push(data);
+    })
+    incoming_message.on("error", (err) =>{
+        console.log(err);
+    })
+    incoming_message.on("end", () =>{
+        const body = Buffer.concat(chunks);
+        let url_result_json = JSON.parse(body);
+        console.log(`This is the url result json ${url_result_json}`);
+        post_image_to_imgur(url_result_json);
+    })
+})
 
-    // const shibe_img_req = https.request(image_url, (incoming_message) =>{
-	// 	const chunks = []
-	// 	incoming_message.on("data", (data) =>{
-	// 		chunks.push(data);
-	// 	});
+shibe_img_request.end();
 
-	// 	incoming_message.on('error', (err) =>{
-	// 		console.log(error);
-	// 	});
 
-	// 	incoming_message.on("end", () =>{
-	// 		const body = Buffer.concat(chunks);
-	// 		let search_result_json = JSON.parse(body);
-	// 		let album_items_length = search_result_json.albums.items.length;
-	// 		let album_items = search_result_json.albums.items;
 
-	// 		// asynchronously display the image 
-	// 		for(let i = 0 ; i < album_items_length; i++){
-	// 			display_image(res, album_items[i].images[0].url, album_items_length);
-	// 		}
-	// 	});
-	// });
+function post_image_to_imgur(url_result_json){
+    console.log(url_result_json);
+    let url_result_string = url_result_json.join("");
+    console.log(url_result_string);
+    const options = {
+        "method": "POST",
+        "hostname": "api.imgur.com",
+        "port": null,
+        "path": "/3/upload",
+        "headers": {
+          "Authorization": "Bearer 80810473b84f865cf5860f3aa628d0c4e4e0dd32",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Content-Length": "0"
+        }
+    };
+
+    const req = https.request(options, function (res) {
+        const chunks = [];
+      
+        res.on("data", function (chunk) {
+          chunks.push(chunk);
+        });
+
+        res.on("error", (err) =>{
+            console.log(err);
+        })
+      
+        res.on("end", function () {
+          const body = Buffer.concat(chunks);
+          console.log(body.toString());
+        });
+    });
+      
+
+    req.write(qs.stringify({
+        type: 'url',
+        image: "https://cdn.shibe.online/shibes/4fb68c74afa0cce1f5e329f6359cf4f0279cc54b.jpg"
+    }), () => req.end());
+    
+}
+
+
