75ed0ea94c08f65712b88ae7984e9ab37a9ab9e9
Hasanat Jahan <jahanhasanat@gmail.com>
2020-12-10 11:03:30 -0500

    the auth is workong without the cookie

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ const fs = require("fs");
 const url = require("url");
 const querystring = require("querystring");
 const credentials = require("./auth/credentials.json");
+const host = "localhost";
 
 server.on("request", connection_handler);
+
 
 function connection_handler(req, res){
     console.log(`New Request for ${req.url} from ${req.socket.remoteAddress}`);
@@ -36,9 +38,19 @@ function connection_handler(req, res){
     // here it is if the button is clicked 
     else if(req.url.startsWith("/post_shibe_image")){
         redirect_to_imgur(res);
+        return
     }   
 
+    // if the code is received 
+	if(req.url.startsWith("/receive_code")){
+        console.log(req.url);
+        const code = url.parse(req.url, true).query;
+        // console.log(`This is the code ${code}`)
+        // receieved_authentication(code, res);
+    }
+
 } //end of connection_handler
+
 
 // this method first checks if you have cached or not - 
 // but no then send a new request 
@@ -56,15 +68,18 @@ function redirect_to_imgur(res){
     const authorization_location = `${authorization_endpoint}?${uri}`;
     console.log(authorization_location);
 
-    res.writeHead(302, {Location: authorization_location}).end();
+    res.writeHead(302, {'Location': authorization_location});
+    res.end();
     
 } // end of redirect_to_imgur 
 
-function receieved_authentication(message, res){
-    console.log("inside recieved authenticaiton");
-    let auth_token = JSON.parse(message);
-    console.log(` This is the auth token${auth_token}`);
-}
+// function receieved_authentication(message, res){
+//     console.log("inside recieved authenticaiton");
+//     // let auth_token = JSON.parse(message);
+//     const queryObject = url.parse(res.url,true).query;
+//     console.log(queryObject);    
+//     // console.log(`This is the auth access token ${auth_token}`);
+// }
 
 server.on("listening", listening_handler);
 function listening_handler(){
